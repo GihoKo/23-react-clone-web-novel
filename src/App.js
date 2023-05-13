8a15a84f149e2913bf8d69968b1a03d1973f4608
giho <giho7427@daum.net>
2023-05-14 03:09:48 +0900

    메인페이지 2차

--- src/App.js
+++ src/App.js
@@ -48,15 +48,14 @@ function App() {
             </div>
             <div className="verticalAlignment">
               <div className="navbar-top-4-toggleBtn ">
-                <button onClick={()=>{
+                <button onClick={() => {
 
                 }}>
                   <FontAwesomeIcon icon={faUser} style={{ fontSize: "24px" }} />
                 </button>
-                <button onClick={()=>{
-                  
+                <button onClick={() => {
+
                 }}>
-                  <FontAwesomeIcon icon={faAngleDown} />
                 </button>
               </div>
             </div>
@@ -82,7 +81,35 @@ function App() {
         <button className="navbar-bottom-list-buttonCss">여성인기</button>
         <button className="navbar-bottom-list-buttonCss">장르전체</button>
       </div>
+      <div className="boidSpace"></div>
+      <div className="main">
+        <div className="main-advertisement1">
+          <Link to="/event/70eaffed97d54ecb9ea3c0cf2bcca789">웹툰 데못죽 시즌2 컴백!</Link>
+        </div>
+        <div className="main-content1-container">
+          <div className="main-content1">
+            <div><img className="main-content1-images" src={"https://dn-img-page.kakao.com/download/resource?kid=b1Tdg9/hzXasDymck/J0yRLkekjBbI2z61PnXnqK&filename=th3"} /></div>
+            <div><img className="main-content1-images" src={"https://dn-img-page.kakao.com/download/resource?kid=cBMB1N/hzMT5dKY1I/PZTkduzWUH1YTGRAKP7wx1&filename=th3"} /></div>
+            <div><img className="main-content1-images" src={"https://dn-img-page.kakao.com/download/resource?kid=b0V5Zb/hzVqD1EAmO/rwvgIbdc1LX4yH129Hdvb0&filename=th3"} /></div>
+            <div><img className="main-content1-images" src={"https://dn-img-page.kakao.com/download/resource?kid=5f0EZ/hAd4VoLtJX/SzS0Pj6Ogxl3PK0Z3gEwMk&filename=th3"} /></div>
+            <div><img className="main-content1-images" src={"https://dn-img-page.kakao.com/download/resource?kid=cd2S1s/hAd4f8Ckmq/rmFrUgmMld3PojKU8nJkd1&filename=th3"} /></div>
+          </div>
+        </div>
 
+        <div className="main-content2-container">
+          <div><img src={"https://dn-img-page.kakao.com/download/resource?kid=bF7Qzn/hyzT0jnEqg/5HdFNwUCkAYFdlySESoxU0&filename=th3"} /></div>
+          <div><img src={"https://dn-img-page.kakao.com/download/resource?kid=vIfzm/hAdNV3EBUI/mB5fSz9QKmhQlgkKXKBidk&filename=th3"} /></div>
+          <div><img src={"https://dn-img-page.kakao.com/download/resource?kid=bcZq24/hzCs6VHJGe/b9S2ziTbwEPcVvSQ0rrZr0&filename=th3"} /></div>
+          <div><img src={"https://dn-img-page.kakao.com/download/resource?kid=dXoLM/hAdNRmEavx/rbmcN6ihhxslzelwI1dnc1&filename=th3"} /></div>
+          <div><img src={"https://dn-img-page.kakao.com/download/resource?kid=cm1RcI/hzN2kVDxFO/OB0DAkrrtzUFL1LnEA2rO0&filename=th3"} /></div>
+          <div><img src={"https://dn-img-page.kakao.com/download/resource?kid=e4thK/hzVqH33HpM/NnuAvxrtdR7UDBpWzr5P71&filename=th3"} /></div>
+        </div>
+
+        <div className="main-advertisement2">
+          <Link to="/event/70eaffed97d54ecb9ea3c0cf2bcca789">충격! 유재석 재입대? 5/16 플유 4화 대공개</Link>
+        </div>
+
+      </div>
 
 
 
